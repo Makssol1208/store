7c0f48527e6dd485ed3981e724c5ea157f48cb7f
Maksym Soloviov <maxim.sv143@gmail.com>
2025-02-25 17:04:58 +0200

    added getRelatedProducts to ProductsSlice

--- src/components/Products/SingleProduct.jsx
+++ src/components/Products/SingleProduct.jsx
@@ -1,12 +1,19 @@
 import React, { useEffect } from "react";
 
+import { ROUTES } from "../../utils/routes";
 import { useNavigate, useParams } from "react-router-dom";
 import { useGetProductQuery } from "../../store/api/apiSlice";
+import { useDispatch, useSelector } from "react-redux";
 
-import { ROUTES } from "../../utils/routes";
+import { getRelatedProducts } from "../../store/products/productsSlice";
+
 import Product from "./Product";
+import Products from "./Products";
 
 export default function SingleProduct() {
+
+  const dispatch = useDispatch();
+  const { related } = useSelector(({ products }) => products);
   const { id } = useParams();
   const navigate = useNavigate();
 
@@ -18,9 +25,16 @@ export default function SingleProduct() {
     }
   }, [isLoading, isFetching, isSuccess, navigate]);
 
+  useEffect(() => {   
+    if (data) {
+      dispatch(getRelatedProducts(data.category.id));
+    }
+  }, [data,dispatch]);
+
   return (
     <>
       <Product {...data} />
+      <Products products={related} amount={5} title="Related Products" />
     </>
   );
 }
